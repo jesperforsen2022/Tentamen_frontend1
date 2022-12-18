1d91ec14ef987ea9342e4780edde8a24d0591c0e
jesperforsen2022 <lester_orkester@hotmail.com>
2022-12-18 22:19:25 +0100

    klar

--- uppgift5/myScript.js
+++ uppgift5/myScript.js
@@ -18,15 +18,3 @@ function createList(){
     orderedList.appendChild(myList);
 
 }
-/*
-
-2. Låt funktionen generera en lista som innehåller ett 
-slumpat antal element mellan 1 och 10. 
-Värdet på dessa element ska också vara slumpade fast mellan 0-100. 
-Exempel på 2 listor som kan bli genererade är: [0, 77, 26] 
-och [22, 1, 78, 100, 0, 8, 10]
-
-3. Skriv ut dessa i en numrerad lista under diven med id:t 
-"resultat". Använd taggarna <ol> och <li> för detta.
-
-*/
